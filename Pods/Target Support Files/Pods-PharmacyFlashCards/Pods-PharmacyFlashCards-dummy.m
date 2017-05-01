545a1686b2824a55fd329db8aa1e3e0ef4ad8729
efwong <edwinfwong@gmail.com>
2017-04-30 19:36:49 -0700

    Hopefully this fixes things

--- Pods/Target Support Files/Pods-PharmacyFlashCards/Pods-PharmacyFlashCards-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PharmacyFlashCards : NSObject
-@end
-@implementation PodsDummy_Pods_PharmacyFlashCards
-@end
---
+++ Top300AppliedDrugsTests/QuestionTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuestionTests.swift
+//  Top300AppliedDrugs
+//
+//  Created by Edwin Wong on 4/30/17.
+//  Copyright © 2017 Edwin Wong. All rights reserved.
+//
+
+import XCTest
+@testable import Top300AppliedDrugs
+
+class QuestionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSimpleQuestion() {
+        //let testQuestion = Question(questionType: QuestionType.brandName, correctDrugIndex: 0, drugAnswers: <#T##[Drug]#>)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
